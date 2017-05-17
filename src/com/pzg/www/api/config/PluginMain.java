ade08a73a4a9097892e8410f6baf501667afed40
TJPlaysNow <pannebakertj@gmail.com>
2017-05-17 18:30:50 -0500

    The official release version
    Made things that made things stop making things

--- src/com/pzg/www/api/config/PluginMain.java
+++ src/com/pzg/www/api/config/PluginMain.java
@@ -2,15 +2,15 @@ package com.pzg.www.api.config;
 import org.bukkit.plugin.java.JavaPlugin;
 public class PluginMain extends JavaPlugin {
 	/** Creating a new config */
-	Config config;
+//	Config config;
 	@Override
 	public void onEnable() {
-		/** Setting up the config */
-		config = new Config(/** Config Path */"plugins/config", /** Config file name */"config.yml", /** The creation method */ new ConfigCreate() {
-			@Override
-			public void configCreate() {
-				/** Stuff happens here */
-			}
-		}, /** The default Plugin class */ this);
+//		/** Setting up the config */
+//		config = new Config(/** Config Path */"plugins/config", /** Config file name */"config.yml", /** The creation method */ new ConfigCreate() {
+//			@Override
+//			public void configCreate() {
+//				/** Stuff happens here */
+//			}
+//		}, /** The default Plugin class */ this);
 	}
 }
